e4e53c1555dae5b4665bccd2f7ccf0c99a738e6d
Ansari Irshad <133130589+AFORBALL-786@users.noreply.github.com>
2023-05-12 23:09:24 +0545

    Add files via upload

---
+++ Day -2 Array.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    // array declare
+    int arr[5];
+    cout << "Array create sucessfully:" << endl;
+    cout << "Address of Array is :" << endl;
+    cout << arr;
+
+    //array initalization
+    int arr[]={2,3,5,6,7};
+    int brr[10] = {1,2,4,5};
+    cout << "Array initalized sucessfully" << endl;
+
+    // // printing all values
+    int arr[] = {2,4,6,7,9};
+
+    for (int index = 0; index < 5; index++)
+    {
+        cout << arr[index] << " ";
+    }
+    
+    // taking input from user
+    int arr[5];
+
+    cout << "Enter The values in array " << endl;
+
+    for (int index = 0; index < 5; index++)
+    {
+        cin >> arr[index];
+    }
+
+    // // printing all values
+    cout << "Printing the values of array " << endl;
+    for (int index = 0; index < 5; index++)
+    {
+        cout << arr[index] << " ";
+    }
+
+    // Question taking input from user and printing their double
+    int arr[5];
+
+    // // taking input
+    cout << "Enter the input value in array" << endl;
+    for (int  i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    // printing their double
+    cout << "Require values is" << endl;
+    for (int  i = 0; i < 5; i++)
+    {
+        cout << 2*arr[i] << " ";
+    }
+
+    // Question taking input from user and intalizing all element by 1.
+    int arr[5];
+    cout << "Enter the input value " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+        arr[i] = 1;
+    }
+
+    cout << "Require answer is " << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    
+    // checking the values which is not initized
+    int arr[5] = {4,7};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    //initiliazing all element by 0 
+    int arr[5] = {0};
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }    
+    
+}
